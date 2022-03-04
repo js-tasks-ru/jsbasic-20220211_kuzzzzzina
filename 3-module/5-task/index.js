2f865fdc63d748aca15ae595047fb040261ab997
Kuzzzzzina <kuzzzzzina@yandex.ru>
2022-03-04 15:06:05 +0300

    Задачи 1, 2, 3, 5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+  let array = str.split(' ');
+ 
+  let min = +array[0];
+  let max = min;
+  for (let i = 0; i < array.length; i++) {
+    let e = +array[i];
+    if (e < min) min = e;
+    if (e > max) max = e;
+  }
+  return {min: min, max: max};
 }
