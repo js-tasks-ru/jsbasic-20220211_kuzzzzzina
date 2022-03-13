042a822797f09bf52340c212828395ffac6a9856
Kuzzzzzina <kuzzzzzina@yandex.ru>
2022-03-13 15:47:12 +0300

    задача 1, 3

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  for (let elem of friends){
+    let li = document.createElement('li');
+    li.innerHTML = `${elem.firstName} ${elem.lastName}`;
+    ul.append(li);
+  }
+  return ul
+ 
 }
+
+
