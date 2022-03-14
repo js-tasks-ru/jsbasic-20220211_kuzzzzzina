c1311ba14f1c0550ecb0868acd222a0ecd77a37f
Kuzzzzzina <kuzzzzzina@yandex.ru>
2022-03-14 15:38:18 +0300

    Задачи 1, 2

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
   // ваш код...
+
+  let button = document.querySelector('.toggle-text-button');
+
+  button.addEventListener("click", handler);
+ 
+  function handler() {
+      text.hidden = !text.hidden;
+  }
+
+  
 }
