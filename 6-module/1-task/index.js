396944563de9aba04d87fa571adfa38ed13b7f96
Kuzzzzzina <kuzzzzzina@yandex.ru>
2022-03-22 14:48:01 +0300

    Таблица с удаляемыми строками

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,51 @@
  *
  */
 export default class UserTable {
+  #rows;
+  #elem;
+
   constructor(rows) {
+      this.#rows = rows;
+      this.#elem = document.createElement("table");
+      this.makeTable();
   }
+  get elem() {
+      return this.#elem;
+  }
+  makeTable() {
+      let elementTable = `      
+      <thead>
+      <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+      </tr>
+      </thead>
+      <tbody>` + this.#rows.map(item => `
+      <tr>
+          <td>${item.name}</td>
+          <td>${item.age}</td>
+          <td>${item.salary}</td>
+          <td>${item.city}</td>
+          <td><button>X</button></td>
+      </tr>              
+              `).join('') + `</tbody>`;
+
+      this.#elem.innerHTML = elementTable;
+      
+      let buttons = this.#elem.querySelectorAll("button");
+      for (let btn of buttons){
+          btn.addEventListener("click", this);
+      }
+  }
+   handleEvent(event) {
+      let row = event.target.parentElement.parentElement; 
+      this.#rows.splice(row.rowIndex - 1, 1); 
+      row.remove();
+      
+  }
+
+
 }
